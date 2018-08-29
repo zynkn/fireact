4f551c99209a58f5b4ccd05cedbd6a299a64cf54
zynkn <ywnwalone@naver.com>
2018-08-29 22:08:42 +1000

    tag attribute edit

--- src/components/Common/Loading/Loading.js
+++ src/components/Common/Loading/Loading.js
@@ -9,7 +9,7 @@ const cx = classNames.bind(styles);
 const Loading = () => (
   <div className={cx("loader")}>
     <svg className={cx("circular")}>
-      <circle className={cx("path")} cx="50" cy="50" r="20" fill="none" stroke-width="5" stroke-miterlimit="10"></circle>
+      <circle className={cx("path")} cx="50" cy="50" r="20" fill="none" strokeWidth="5" strokeMiterlimit="10"></circle>
     </svg>
   </div>
 );
