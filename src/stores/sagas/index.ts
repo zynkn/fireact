58fcf9a962b0f3e4d01eee854b09c4b88ae9dc77
zynkn <wls5563@gmail.com>
2020-02-17 19:26:29 +0900

    amplify init

--- src/stores/sagas/index.ts
+++
@@ -1,9 +0,0 @@
-import { all } from 'redux-saga/effects'
-import workoutSaga from './workoutSaga';
-import userSaga from './userSaga';
-export default function* roodSaga() {
-  yield all([
-    workoutSaga(),
-    userSaga(),
-  ])
-}
