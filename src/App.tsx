73fff1f750f1d8db6358a60a4032ff2403d61cbd
zynkn <wls5563@gmail.com>
2020-05-19 17:04:37 +0900

    last try

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css';
 
-const App = () => {
+const App:React.FC<any> = () => {
   return (
     <div>
       <h2>Amplify Todos</h2>
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import Amplify from "aws-amplify";
-// import awsExports from "./aws-exports";
-// Amplify.configure(awsExports);
 
 ReactDOM.render(
   <React.StrictMode>
