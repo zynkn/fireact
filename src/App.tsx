754908759b5710a615f0cc7019b6565a7ab6b4d2
zynkn <wls5563@gmail.com>
2020-05-19 16:08:39 +0900

    without aws-exports.js 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { API, graphqlOperation } from 'aws-amplify'
 import { createTodo } from './graphql/mutations'
 import { listTodos } from './graphql/queries'
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+// import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 const initialState = { name: '', description: '' }
 
@@ -42,7 +42,6 @@ const App = () => {
   return (
     <div style={styles.container}>
       <h2>Amplify Todos</h2>
-      <AmplifySignOut />
       <input
         onChange={event => setInput('name', event.target.value)}
         style={styles.input}
@@ -77,4 +76,5 @@ const styles:any = {
   button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
 }
 
-export default withAuthenticator(App);
+//export default withAuthenticator(App);
+export default App;
